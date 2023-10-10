9c073019e50a4dd2c82e1bae1b7a31efbd907c66
Dmytro <dsumcow@gmail.com>
2023-10-10 23:47:38 +0300

    Added first file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter product price: ");
+            int price = int.Parse(Console.ReadLine());
+            Console.Write("Enter product name: ");
+            string productName = Console.ReadLine();
+
+            Console.WriteLine($"\nProduct name: {productName}\nProduct price: {price}");
+        }
+    }
+}
